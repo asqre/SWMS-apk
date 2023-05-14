4c04a5ade3e2dca596e2d1a70d5fc0430b0b4e7a
asqre <amita.ic.19@nitj.ac.in>
2023-05-14 20:02:13 +0530

    testing successful

--- lib/main.dart
+++ lib/main.dart
@@ -68,11 +68,3 @@ class SWMS extends StatelessWidget {
       );
 }
 
-class Testing extends StatelessWidget {
-  const Testing({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
